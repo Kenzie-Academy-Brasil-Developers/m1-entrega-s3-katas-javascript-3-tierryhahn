8137940ccec610e0510c32bd992a4b767dfa8eed
tierryhahn <tierryhahn@gmail.com>
2022-03-10 14:14:04 -0300

    JS atualizado

--- katas3.js
+++ katas3.js
@@ -1,7 +1,7 @@
 const sampleArray = [469, 755, 244, 245, 758, 450, 302, 17, 712, 71, 456, 21, 398, 339, 882, 848, 179, 535, 940, 472];
 
 function showResults (num) {
-    console.log(num)
+    return num
 }
 
 function kata1() {
